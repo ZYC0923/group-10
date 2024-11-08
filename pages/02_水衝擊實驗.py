903a79f0e1b264ce2c7a12df16d7aad9afa364f7
ZYC0923 <91449878+ZYC0923@users.noreply.github.com>
2024-11-08 19:34:41 +0800

    Add files via upload

--- pages/02_水衝擊實驗.py
+++ pages/02_水衝擊實驗.py
@@ -4,4 +4,17 @@ st.title("實驗二: 水衝擊實驗")
 
 st.write("這裡是水衝擊實驗的詳細內容。")
 
+file_paths = {
+    "doc": "file/實驗2.水衝擊實驗.docx",
+}
+def create_download_button(file_path, label, mime_type):
+    with open(file_path, "rb") as f:
+        file_content = f.read()
+    st.download_button(
+        label=label,
+        data=file_content,
+        file_name=file_path.split("/")[-1],  # 檔案名稱
+        mime=mime_type
+    )
+
 # 在這裡添加實驗一的具體內容，如圖表、數據等
--- pages/03_柏努利文氏管實驗.py
+++ pages/03_柏努利文氏管實驗.py
@@ -4,4 +4,16 @@ st.title("實驗三: 柏努利文氏管實驗")
 
 st.write("這裡是柏努利文氏管實驗的詳細內容。")
 
+file_paths = {
+    "doc": "file/伯努利文氏管實驗.docx",
+}
+def create_download_button(file_path, label, mime_type):
+    with open(file_path, "rb") as f:
+        file_content = f.read()
+    st.download_button(
+        label=label,
+        data=file_content,
+        file_name=file_path.split("/")[-1],  # 檔案名稱
+        mime=mime_type
+    )
 # 在這裡添加實驗一的具體內容，如圖表、數據等
