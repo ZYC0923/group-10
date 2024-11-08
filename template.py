e7f3588a0203a27f7d0077187664f009860b241b
ZYC0923 <91449878+ZYC0923@users.noreply.github.com>
2024-11-08 18:29:35 +0800

    Add files via upload

---
+++ template.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(page_title="熱流力實驗", layout="wide")
+
+st.title("熱流力實驗第10組")
+
+st.write("歡迎來到熱流力實驗報告。請從左側選單選擇要查看的實驗項目。")
+
+st.write("指導教授:周榮源教授")
+
+st.write("班級:四設四乙")
+
+st.write("組員:41023252 鄭煜橙、41023210 鄭翊均、41023222 陳奕倫、41023242 廖旭宏、41023255 徐佑寧")
+
+# 不需要在這裡定義實驗項目列表，因為Streamlit會自動生成側邊欄
